d350ff301b1c550848aeab5e756922f0c35fec8d
Houlet <chhoulet@yahoo.fr>
2017-05-10 17:58:16 +0200

    Initialisation du projet + FOSUserBundle

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+       parent::__construct();
+       // your own logic
+    }
+}
