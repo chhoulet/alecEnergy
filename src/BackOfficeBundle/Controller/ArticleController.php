1b003b15ec9a667bb0599dc27de344cdc2693356
Houlet <chhoulet@yahoo.fr>
2017-05-12 12:29:33 +0200

    Fonctions Activer/Désactiver un article

--- src/BackOfficeBundle/Controller/ArticleController.php
+++ src/BackOfficeBundle/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FrontOfficeBundle\Entity\Article;
 use FrontOfficeBundle\Form\ArticleType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ArticleController extends Controller
 {
@@ -46,7 +47,7 @@ class ArticleController extends Controller
 			$em->persist($article);
 			$em->flush();
 
-			$session->getFlashBag()->add('success', $article->getTitle() . ' est bien créé et ajouté en base !');
+			$session->getFlashBag()->add('succes', $article->getTitle() . ' est bien créé et ajouté en base !');
 			return $this->redirectToRoute('back_office_homepage');
 		}
 
@@ -55,5 +56,74 @@ class ArticleController extends Controller
 				  'origin'=>$origin));
 	}
 
+	public function activateAction(Request $request, $idArticle)
+	{
+		$em=$this->getDoctrine()->getManager();
+		$session=$request->getSession();
 
+		if($idArticle)
+		{
+			$listArticles=$em->getRepository('FrontOfficeBundle:Article')->findByActive(2);
+			$listIdArticles=[];
+			foreach($listArticles as $article)
+			{
+				$id=$article->getId();
+				$listIdArticles[]=$id;
+			}
+
+			if(in_array($idArticle, $listIdArticles))
+			{
+				$articleActivated=$em->getRepository('FrontOfficeBundle:Article')->find($idArticle);
+				$articleActivated->setActive(1);
+				$em->flush();
+
+				$session->getFlashBag()->add('succes', $articleActivated->getTitle(). ' est activé et disponible dans l\'espace client !');
+				return $this->redirectToRoute('back_office_homepage');
+			}
+			else
+			{
+				throw new NotFoundHttpException("Cet article n\'existe pas !");
+			}
+		}
+		else
+		{
+			throw new NotFoundHttpException("Cet article n\'existe pas !");
+		}	
+	}
+
+
+	public function desactivateAction(Request $request, $idArticle)
+	{
+		$em=$this->getDoctrine()->getManager();
+		$session=$request->getSession();
+
+		if($idArticle)
+		{
+			$listArticles=$em->getRepository('FrontOfficeBundle:Article')->findByActive(1);
+			$listIdArticles=[];
+			foreach($listArticles as $article)
+			{
+				$id=$article->getId();
+				$listIdArticles[]=$id;
+			}
+
+			if(in_array($idArticle, $listIdArticles))
+			{
+				$articledesActivated=$em->getRepository('FrontOfficeBundle:Article')->find($idArticle);
+				$articledesActivated->setActive(2);
+				$em->flush();
+
+				$session->getFlashBag()->add('succes', $articledesActivated->getTitle(). ' est désactivé et retiré de l\'espace client !');
+				return $this->redirectToRoute('back_office_homepage');
+			}
+			else
+			{
+				throw new NotFoundHttpException("Cet article n\'existe pas !");
+			}
+		}
+		else
+		{
+			throw new NotFoundHttpException("Cet article n\'existe pas !");
+		}	
+	}
 }
